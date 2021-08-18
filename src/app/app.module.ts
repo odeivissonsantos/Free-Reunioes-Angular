0eef858ca19e0d0705fb1775a06763ca5584450b
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-18 00:31:03 -0300

    atualizado a url base e inserido o modulo http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,11 +23,10 @@ import { ReunioesDetailsComponent } from './components/reunioes/reunioes-details
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [
-    HttpClient
-  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https:/api-free-reunioes.herokuapp.com/'
+  baseUrl: 'https:/api-free-reunioes.herokuapp.com'
 };
 
 /*
