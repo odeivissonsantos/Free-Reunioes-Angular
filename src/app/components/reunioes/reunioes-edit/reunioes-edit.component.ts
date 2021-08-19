f2fa8eeb06e539d71964c141beac494d90d72363
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-19 11:59:37 -0300

    CRUD finalizado para reunioes

--- src/app/components/reunioes/reunioes-edit/reunioes-edit.component.ts
+++ src/app/components/reunioes/reunioes-edit/reunioes-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Reuniao } from '../reuniao.model';
+import { ReuniaoService } from '../reuniao.service';
 
 @Component({
   selector: 'app-reunioes-edit',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reunioes-edit.component.css']
 })
 export class ReunioesEditComponent implements OnInit {
+  reuniao: Reuniao = {
+    nome: '',
+    data: '',
+    horaInicio: '',
+    horaTermino: ''
+  }
 
-  constructor() { }
+  constructor(private service: ReuniaoService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.reuniao.id! = this.route.snapshot.paramMap.get('id')!
+    this.buscarPorId()
+  }
+
+  atualizar(): void {
+    this.service.atualizar(this.reuniao).subscribe((resposta) => {
+      this.router.navigate(['reunioes-list'])
+    })
+  }
+
+  buscarPorId(): void {
+    this.service.buscarPorId(this.reuniao.id!).subscribe((resposta) => {
+      this.reuniao = resposta
+    })
+  }
+
+  voltar(): void {
+    this.router.navigate(['reunioes-list'])
   }
 
 }
